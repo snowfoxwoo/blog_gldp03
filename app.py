ccbfa827a6671958dc926c9e711d40bec5c94d0a
Derrick Woo <derrickwoo@infa.com>
2020-07-09 01:24:26 +0800

    added python tornado script

---
+++ app.py
@@ -0,0 +1,27 @@
+import tornado.ioloop
+import tornado.web
+
+import redis
+import socket
+
+class MainHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.database = redis.Redis(host='localhost', port=6379, db=0)
+        visitCount = int(self.database.get('counter'))
+        visitCount = visitCount + 1
+        hostName = socket.gethostname()
+        ipAddr = socket.gethostbyname(hostName)
+        self.database.set("counter", visitCount)
+        self.write("Hello! This page is visited %d time(s)<br>current recorded IP is %s" % (visitCount, ipAddr))
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
